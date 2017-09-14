f3f02df100f6372c07ceef1fc96d042072df12f5
Cooper Yao <309318267@qq.com>
2017-09-14 15:54:51 +0800

    实例

---
+++ exercise.js
@@ -0,0 +1,36 @@
+var student = new Object(); 
+student.name = "Lanny"; 
+student.age = "25"; 
+student.location = "China"; 
+var json = JSON.stringify(student); 
+alert(json); 
+
+//{"name":"Lanny","age":"25","location":"China"}
+
+var students = new Array() ; 
+students[0] = "onepiece"; 
+students[1] = "naruto"; 
+students[2] = "bleach"; 
+var json = JSON.stringify(students,switchUpper); 
+function switchUpper(key, value) { 
+    return value.toString().toUpperCase(); 
+} 
+alert(json); 
+//{"ONEPIECE,NARUTO,BLEACH"}
+
+var stuObj = new Object(); 
+stuObj.id = "20122014001"; 
+stuObj.name = "Tomy"; 
+stuObj.age = 25; 
+
+var stuArr = new Array(); 
+stuArr[0] = "id"; 
+stuArr[1] = "age"; 
+stuArr[2] = "addr";//这个stuObj对象里不存在。 
+
+var json = JSON.stringify(stuObj,stuArr); 
+//var json = JSON.stringify(stuObj,stuArr,1000); 
+//var json = JSON.stringify(stuObj,stuArr,'\t'); 
+//var json = JSON.stringify(stuObj,stuArr,'OK '); 
+alert(json); 
+//{"id":"20122014001","age":"25"}
